42b4c5fa04c2a9ebec486666474fb0d9794fce20
kaiser <1244713586@qq.com>
2019-12-13 02:41:30 +0800

    fix

--- test/sqlite/run-speed-test.sh
+++ test/sqlite/run-speed-test.sh
@@ -21,11 +21,7 @@ then
 fi
 NAME=$1
 shift
-CC_OPTS="-DSQLITE_DEFAULT_MEMSTATUS=0 -DSQLITE_DQS=0 -DSQLITE_ENABLE_DBSTAT_VTAB
-    -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_GEOPOLY -DSQLITE_ENABLE_JSON1
-    -DSQLITE_ENABLE_RBU -DSQLITE_ENABLE_RTREE -DSQLITE_LIKE_DOESNT_MATCH_BLOBS
-    -DSQLITE_MAX_EXPR_DEPTH=0 -DSQLITE_OMIT_DECLTYPE -DSQLITE_OMIT_DEPRECATED
-    -DSQLITE_USE_ALLOCA -DNDEBUG -DSQLITE_ENABLE_MEMSYS5"
+CC_OPTS="-DSQLITE_ENABLE_RTREE -DSQLITE_ENABLE_MEMSYS5"
 SPEEDTEST_OPTS="--shrink-memory --reprepare --heap 10000000 64"
 SIZE=5
 doExplain=0
@@ -74,15 +70,15 @@ echo "NAME           = $NAME" | tee summary-$NAME.txt
 echo "SPEEDTEST_OPTS = $SPEEDTEST_OPTS" | tee -a summary-$NAME.txt
 echo "CC_OPTS        = $CC_OPTS" | tee -a summary-$NAME.txt
 rm -f cachegrind.out.* speedtest1 speedtest1.db sqlite3.o
-kcc -g -O0 $CC_OPTS -c sqlite3.c
+kcc -O3 -g $CC_OPTS -c sqlite3.c
 size sqlite3.o | tee -a summary-$NAME.txt
 if test $doExplain -eq 1; then
-  kcc -g -O0 $CC_OPTS \
+  kcc -O3 -g $CC_OPTS \
      -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
-    ./shell.c ./sqlite3.c -o sqlite3 -ldl -lpthread -lm
+    ./shell.c ./sqlite3.c -o sqlite3 -ldl -lpthread
 fi
 SRC=./speedtest1.c
-kcc -g -O0 $CC_OPTS $SRC ./sqlite3.o -o speedtest1 -ldl -lpthread -lm
+kcc -O3 -g $CC_OPTS $SRC ./sqlite3.o -o speedtest1 -ldl -lpthread
 ls -l speedtest1 | tee -a summary-$NAME.txt
 valgrind --tool=cachegrind ./speedtest1 speedtest1.db \
     $SPEEDTEST_OPTS 2>&1 | tee -a summary-$NAME.txt
